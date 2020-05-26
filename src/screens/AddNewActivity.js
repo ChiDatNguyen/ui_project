97b0d60ced42dfe619f95a6509eea95e1d785c27
nguyensiviet1999 <36764859+nguyensiviet1999@users.noreply.github.com>
2020-05-26 22:17:41 +0700

    commit sửa hết bug rồi

--- src/screens/AddNewActivity.js
+++ src/screens/AddNewActivity.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import { ScrollView,Text, View, Image, AppRegistry, Button,Navigator,StyleSheet,TouchableOpacity } from 'react-native';
 import { CheckBox,Input } from 'react-native-elements';
-// import Icon from 'react-native-vector-icons/FontAwesome';
-// import ScheduleScreen from './ScheduleScreen';
 import RNPickerSelect from 'react-native-picker-select';
-import DateTimePicker from '@react-native-community/datetimepicker';
+// import DateTimePicker from '@react-native-community/datetimepicker';
 
 export class AddNewActivity extends React.Component{
     constructor(props) {
@@ -36,17 +34,7 @@ export class AddNewActivity extends React.Component{
                             { label: 'Gym', value: 'baseball' }
                         ]}
                     />
-                    {/* <Input
-                        placeholder='input Your Activity'
-                        leftIcon={
-                            <Icon
-                            name='user'
-                            size={24}
-                            color='black'
-                            />
-                        }
-                        onChangeText={value => this.setState({ activity: value })}
-                    /> */}
+
                 </View>
                 <View>
                     <Text>Ngày thực hiện</Text>
@@ -54,50 +42,50 @@ export class AddNewActivity extends React.Component{
                         title='Thứ 2'
                         checked={this.state.T2}
                         onPress={() => this.setState({T2: !this.state.T2})}
-                        checkedIcon={<Image source={require('../../images/checked.png')} style={{height:20, width:20}} />}
-                        uncheckedIcon={<Image source={require('../../images/unchecked.png')} style={{height:20, width:20}}/>}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{height:20, width:20}} />}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{height:20, width:20}}/>}
                     />
                     <CheckBox
                         checked={this.state.T3}
                         title='Thứ 3'
                         onPress={() => this.setState({T3: !this.state.T3})}
-                        checkedIcon={<Image source={require('../../images/checked.png')} style={{height:20, width:20}}/>}
-                        uncheckedIcon={<Image source={require('../../images/unchecked.png')} style={{height:20, width:20}}/>}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{height:20, width:20}}/>}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{height:20, width:20}}/>}
                     />
                     <CheckBox
                         checked={this.state.T4}
                         title='Thứ 4'
                         onPress={() => this.setState({T4: !this.state.T4})}
-                        checkedIcon={<Image source={require('../../images/checked.png')} style={{height:20, width:20}} />}
-                        uncheckedIcon={<Image source={require('../../images/unchecked.png')} style={{height:20, width:20}}/>}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{height:20, width:20}} />}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{height:20, width:20}}/>}
                     />
                     <CheckBox
                         checked={this.state.T5}
                         title='Thứ 5'
                         onPress={() => this.setState({T5: !this.state.T5})}
-                        checkedIcon={<Image source={require('../../images/checked.png')} style={{height:20, width:20}}/>}
-                        uncheckedIcon={<Image source={require('../../images/unchecked.png')} style={{height:20, width:20}}/>}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{height:20, width:20}}/>}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{height:20, width:20}}/>}
                     />
                     <CheckBox
                         checked={this.state.T6}
                         title='Thứ 6'
                         onPress={() => this.setState({T6: !this.state.T6})}
-                        checkedIcon={<Image source={require('../../images/checked.png')} style={{height:20, width:20}}/>}
-                        uncheckedIcon={<Image source={require('../../images/unchecked.png')} style={{height:20, width:20}}/>}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{height:20, width:20}}/>}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{height:20, width:20}}/>}
                     />
                     <CheckBox
                         checked={this.state.T7}
                         title='Thứ 7'
                         onPress={() => this.setState({T7: !this.state.T7})}
-                        checkedIcon={<Image source={require('../../images/checked.png')} style={{height:20, width:20}}/>}
-                        uncheckedIcon={<Image source={require('../../images/unchecked.png')} style={{height:20, width:20}}/>}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{height:20, width:20}}/>}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{height:20, width:20}}/>}
                     />
                     <CheckBox
                         checked={this.state.CN}
                         title='Chủ Nhật'
                         onPress={() => this.setState({CN: !this.state.CN})}
-                        checkedIcon={<Image source={require('../../images/checked.png')} style={{height:20, width:20}}/>}
-                        uncheckedIcon={<Image source={require('../../images/unchecked.png')} style={{height:20, width:20}}/>}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{height:20, width:20}}/>}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{height:20, width:20}}/>}
                     />
                 </View>
                 <View>
@@ -105,21 +93,10 @@ export class AddNewActivity extends React.Component{
                     <Input
                         placeholder='hour:minute'
                         leftIcon={
-                            <Image source={require('../../images/run_icon.png')} style={{width: 20, height: 20}}/>
+                            <Image source={require('../../images/schedule/run_icon.png')} style={{width: 20, height: 20}}/>
                         }
                         onChangeText={value => this.setState({ time: value })}
                     />
-                    {true &&
-                        <DateTimePicker
-                            timeZoneOffsetInMinutes={0}
-                            value= {this.setState.time}
-                            mode='time'
-                            is24Hour={true}
-                            display="default"
-                            onChange={value => this.setState({ time: value })}
-                            show={true}
-                        />
-                    }
                 </View>
                 <View>
                 <Button
--- src/screens/ScheduleScreen.js
+++ src/screens/ScheduleScreen.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Text, View, Image, AppRegistry, Button,Navigator,ScrollView,StyleSheet,TouchableOpacity,SafeAreaView,FlatList } from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { AddNewActivity } from './AddNewActivity';
@@ -18,53 +17,16 @@ export class ScheduleScreen extends React.Component{
     }
 }
 class ListActivity extends React.Component{
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { 
-    //         thu:[false,true,false,true,true,false,false],
-    //         activity:"",
-    //         time:'20:00'
-    //     };
-    //   }
-    // renderItem = () => {
-    //     let allThu = "";
-    //     for (let index = 0; index < this.state.thu; index++) {
-    //         if(this.state.thu[index]==true){
-    //             allThu+=(string)(index);
-    //             allThu+=" ";
-    //         }
-    //     }
-    //     return (
-    //         <View style={style.activelist}>
-    //             <Image 
-    //                 style={style.imageactive}
-    //                 source={require('../../images/assets/run_icon.png')}
-    //                 style={{width: 40, height: 40}} 
-    //             />
-    //             <View style={style.active}>
-    //                 <Text style={style.activename}>
-    //                     {this.state.activity}
-    //                 </Text>
-    //                 <Text style={style.activeDate}>
-    //                     {allThu}
-    //                 </Text>
-    //             </View>
-    //             <Text style={style.time}>
-    //                 {this.state.time}
-    //             </Text>
-    //         </View>
-    //     );
-    // }
     render(){
         // const { navigation } = this.props.navigation;
         return(
             <View style={{ flex: 1}}>
-        
+                
                 <TouchableOpacity onPress={()=> this.props.navigation.navigate('add')}>
                     <View style={{ flexDirection:'row-reverse'}}>
                         <Text>Thêm mới</Text>
                         <Image 
-                            source={require('../../images/assets/add.png')} 
+                            source={require('../../images/schedule/add.png')} 
                             style={{width: 20, height: 20}}
                         />
                     </View>
@@ -72,7 +34,7 @@ class ListActivity extends React.Component{
                        <View style={style.activelist}>
                             <Image 
                                 style={style.imageactive}
-                                source={require('../../images/assets/run_icon.png')}
+                                source={require('../../images/schedule/run_icon.png')}
                                 style={{width: 40, height: 40}} 
                             />
                             <View style={style.active}>
@@ -90,7 +52,7 @@ class ListActivity extends React.Component{
                         <View style={style.activelist}>
                             <Image 
                                 style={style.imageactive}
-                                source={require('../../images/assets/gym_icon.png')}
+                                source={require('../../images/schedule/gym_icon.png')}
                                 style={{width: 40, height: 40}} 
                             />
                             <View style={style.active}>
