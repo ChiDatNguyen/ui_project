95cf0c90ec781af72c42be71744a21537053cf6f
nguyensiviet1999 <36764859+nguyensiviet1999@users.noreply.github.com>
2020-05-28 01:15:20 +0700

    đã chay thử rồi . ngon lành mà

---
+++ src/screens/AddNewActivity.js
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import { ScrollView, Text, View, Image, AppRegistry, Button, Navigator, StyleSheet, TouchableOpacity } from 'react-native';
+import { CheckBox, Input } from 'react-native-elements';
+import RNPickerSelect from 'react-native-picker-select';
+// import DateTimePicker from '@react-native-community/datetimepicker';
+
+export class AddNewActivity extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            T2: false,
+            T3: false,
+            T4: false,
+            T5: false,
+            T6: false,
+            T7: false,
+            CN: false,
+            activity: "",
+            time: '20:00'
+        };
+    }
+    xacnhan() {
+        this.props.navigation.goBack();
+    }
+    render() {
+        return (
+            <ScrollView>
+                <View>
+                    <Text>Hoạt động</Text>
+                    <RNPickerSelect
+                        onValueChange={value => this.setState({ activity: value })}
+                        items={[
+                            { label: 'Sport', value: 'football' },
+                            { label: 'Gym', value: 'baseball' }
+                        ]}
+                    />
+
+                </View>
+                <View>
+                    <Text>Ngày thực hiện</Text>
+                    <CheckBox
+                        title='Thứ 2'
+                        checked={this.state.T2}
+                        onPress={() => this.setState({ T2: !this.state.T2 })}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{ height: 20, width: 20 }} />}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{ height: 20, width: 20 }} />}
+                    />
+                    <CheckBox
+                        checked={this.state.T3}
+                        title='Thứ 3'
+                        onPress={() => this.setState({ T3: !this.state.T3 })}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{ height: 20, width: 20 }} />}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{ height: 20, width: 20 }} />}
+                    />
+                    <CheckBox
+                        checked={this.state.T4}
+                        title='Thứ 4'
+                        onPress={() => this.setState({ T4: !this.state.T4 })}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{ height: 20, width: 20 }} />}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{ height: 20, width: 20 }} />}
+                    />
+                    <CheckBox
+                        checked={this.state.T5}
+                        title='Thứ 5'
+                        onPress={() => this.setState({ T5: !this.state.T5 })}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{ height: 20, width: 20 }} />}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{ height: 20, width: 20 }} />}
+                    />
+                    <CheckBox
+                        checked={this.state.T6}
+                        title='Thứ 6'
+                        onPress={() => this.setState({ T6: !this.state.T6 })}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{ height: 20, width: 20 }} />}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{ height: 20, width: 20 }} />}
+                    />
+                    <CheckBox
+                        checked={this.state.T7}
+                        title='Thứ 7'
+                        onPress={() => this.setState({ T7: !this.state.T7 })}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{ height: 20, width: 20 }} />}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{ height: 20, width: 20 }} />}
+                    />
+                    <CheckBox
+                        checked={this.state.CN}
+                        title='Chủ Nhật'
+                        onPress={() => this.setState({ CN: !this.state.CN })}
+                        checkedIcon={<Image source={require('../../images/schedule/checked.png')} style={{ height: 20, width: 20 }} />}
+                        uncheckedIcon={<Image source={require('../../images/schedule/unchecked.png')} style={{ height: 20, width: 20 }} />}
+                    />
+                </View>
+                <View>
+                    <Text>Giờ thông báo</Text>
+                    <Input
+                        placeholder='hour:minute'
+                        leftIcon={
+                            <Image source={require('../../images/schedule/run_icon.png')} style={{ width: 20, height: 20 }} />
+                        }
+                        onChangeText={value => this.setState({ time: value })}
+                    />
+                </View>
+                <View>
+                    <Button
+                        title="Xác Nhận"
+                        onPress={() => {
+                            this.props.navigation.navigate('list')
+                        }
+                        }
+
+                    />
+                </View>
+            </ScrollView>
+        );
+    }
+}
+
+var style = StyleSheet.create({
+});
