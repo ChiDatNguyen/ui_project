d0f76d6e4ac9325c45e7cd4c4cee8146ce019ae9
dzeus14omega <43383923+dzeus14omega@users.noreply.github.com>
2020-06-16 01:03:10 +0700

    add animation and duplicate more link

--- src/screens/AdviceScreen.js
+++ src/screens/AdviceScreen.js
@@ -6,7 +6,9 @@ import {
   ImageBackground,
   Image,
   ScrollView,
-  Dimensions,
+  TouchableOpacity,
+  Linking,
+  Animated,
 } from 'react-native';
 import Rectangle_1 from '../../images/advice/Rectangle_1.png';
 import icon_music from '../../images/advice/icon_music.png';
@@ -17,50 +19,206 @@ import nutrious from '../../images/advice/nutrious.png';
 export function AdviceScreen() {
   return (
     <View>
-      {/* <View>
-        <Text style={styles.title}>Advices</Text>
-      </View> */}
+      <List_extend />
+      <Text style={styles.small_title}>More advices</Text>
       <Advice_main />
     </View>
   );
+}
+
+class Advice_header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {resize_BG: new Animated.Value(0)};
+  }
+
+  render() {
+    const {resize_BG} = this.state;
+    return (
+      <ImageBackground
+        source={Rectangle_1}
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          margin: 20,
+          height: 250,
+          borderRadius: 100,
+        }}
+        // eslint-disable-next-line react-native/no-inline-styles
+        imageStyle={{borderRadius: 30}}>
+        <Text style={styles.content_advice}>
+          You have been sitting for along time! Try another activity?
+        </Text>
+        <View style={styles.border_icon}>
+          <TouchableOpacity>
+            <Image source={icon_music} style={styles.icon} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={icon_run} style={styles.icon} />
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    );
+  }
 }
 
 class Advice_main extends Component {
   render() {
     return (
       <ScrollView>
-        <ImageBackground
-          source={Rectangle_1}
-          style={styles.image}
-          imageStyle={{borderRadius: 40}}>
-          <Text style={styles.content_advice}>
-            You have been sitting for along time! Try another activity?
-          </Text>
-          <View style={styles.border_icon}>
-            <Image source={icon_music} style={styles.icon} />
-            <Image source={icon_run} style={styles.icon} />
-          </View>
-        </ImageBackground>
-
         <View>
-          <Text style={styles.small_title}>More advices</Text>
-
           <View style={styles.list}>
-            <View style={styles.list_child}>
+            <TouchableOpacity
+              style={styles.list_child}
+              onPress={() =>
+                Linking.openURL(
+                  'http://www.coretenfitness.com/tag/fitness-tutorial/',
+                )
+              }>
               <View style={styles.box1}>
-                <Text style={styles.name_tag}>TRAINING TUTORIALS</Text>
+                <Text style={styles.name_tag}>TRAINNING TUTORIALS</Text>
               </View>
               <Image source={traning} style={styles.icon2} />
-            </View>
-            <View style={styles.list_child}>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.list_child}
+              onPress={() =>
+                Linking.openURL('https://nutritionsolutions.com/')
+              }>
               <View style={styles.box2}>
                 <Text style={styles.name_tag}>Nutrious Solution</Text>
               </View>
               <Image source={nutrious} style={styles.icon2} />
-            </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.list_child}
+              onPress={() =>
+                Linking.openURL(
+                  'http://www.coretenfitness.com/tag/fitness-tutorial/',
+                )
+              }>
+              <View style={styles.box1}>
+                <Text style={styles.name_tag}>TRAINNING TUTORIALS</Text>
+              </View>
+              <Image source={traning} style={styles.icon2} />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.list_child}
+              onPress={() =>
+                Linking.openURL('https://nutritionsolutions.com/')
+              }>
+              <View style={styles.box2}>
+                <Text style={styles.name_tag}>Nutrious Solution</Text>
+              </View>
+              <Image source={nutrious} style={styles.icon2} />
+            </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
+    );
+  }
+}
+
+class List_extend extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width_change: new Animated.Value(50),
+      height_change: new Animated.Value(50),
+      fade: new Animated.Value(0),
+      state_anim: false,
+      opacity_icon: 0,
+    };
+  }
+
+  componentDidMount() {
+    Animated.parallel([
+      Animated.timing(this.state.width_change, {
+        toValue: 370,
+        duration: 500,
+      }),
+      Animated.timing(this.state.height_change, {
+        toValue: 100,
+        duration: 500,
+      }),
+      Animated.timing(this.state.fade, {
+        toValue: 1,
+        duration: 300,
+        delay: 450,
+      }),
+    ]).start(() => {});
+  }
+
+  toggle = () => {
+    if (this.state.state_anim == false) {
+      this.setState({opacity_icon: 1});
+      Animated.parallel([
+        Animated.timing(this.state.height_change, {
+          toValue: 250,
+          duration: 500,
+        }),
+      ]).start(() => {});
+      this.state.state_anim = true;
+    } else {
+      this.setState({opacity_icon: 0});
+      Animated.parallel([
+        Animated.timing(this.state.height_change, {
+          toValue: 100,
+          duration: 500,
+        }),
+      ]).start(() => {});
+      this.state.state_anim = false;
+    }
+  };
+
+  render() {
+    const width = this.state.width_change;
+    const height = this.state.height_change;
+    const opacity = this.state.fade;
+    return (
+      <Animated.View
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          width,
+          height,
+          backgroundColor: 'green',
+          margin: 20,
+          borderRadius: 30,
+          alignItems: 'center',
+        }}>
+        <TouchableOpacity onPress={this.toggle}>
+          <Animated.View style={{opacity}}>
+            <Text style={[styles.content_advice]}>
+              You have been sitting for along time! Try another activity?
+            </Text>
+          </Animated.View>
+
+          <View style={styles.border_icon}>
+            <TouchableOpacity>
+              <Image
+                source={icon_music}
+                style={{
+                  width: 100,
+                  height: 100,
+                  opacity: this.state.opacity_icon,
+                }}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                source={icon_run}
+                style={{
+                  width: 100,
+                  height: 100,
+                  opacity: this.state.opacity_icon,
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+        </TouchableOpacity>
+      </Animated.View>
     );
   }
 }
@@ -79,7 +237,8 @@ var styles = StyleSheet.create({
   },
   image: {
     margin: 20,
-    height: 350,
+    height: 250,
+    resizeMode: 'cover',
     borderRadius: 100,
   },
   border_icon: {
